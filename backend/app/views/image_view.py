03f6d54898be20eae454eb2ce7617c3b8d8d2030
danangjoyoo <agus.danangjoyo@gmail.com>
2022-12-10 13:18:34 +0700

    feat: remove unused image endpoints

--- backend/app/views/image_view.py
+++ backend/app/views/image_view.py
@@ -2,31 +2,21 @@
 Image Views
 """
 from flask import jsonify
-from flask_toolkits import Path, Header, Query, Body, File
+from flask_toolkits import Path, Header, Query, Body, File, Form
+from flask_toolkits.responses import JSONResponse
+from typing import Optional
+from werkzeug.datastructures import FileStorage
 
-# from app.controllers.image_controller import
-
-
-def get_image_view():
-    result = None
-    return jsonify(result)
-
-
-def get_image_list_view():
-    result = None
-    return jsonify(result)
+from app.controllers import image_controller as controller
+from app.schema.image_schema import ImageEntityEnum
 
 
-def create_image_view():
-    result = None
-    return jsonify(result)
+def create_image_view(
+    entity_type: ImageEntityEnum = Form(),
+    image: FileStorage = File()
+):
+    result = controller.post_image()
+    return JSONResponse(result)
 
-
-def update_image_view():
-    result = None
-    return jsonify(result)
-
-
-def delete_image_view():
-    result = None
-    return jsonify(result)
+def delete_image_view(image_id: int = Path()):
+    return
