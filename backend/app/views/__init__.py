2f8f55d608670efcca6b09883f0f2faef6d30a6e
danangjoyoo <agus.danangjoyo@gmail.com>
2022-12-10 13:58:32 +0700

    feat: update variant endpoints

--- backend/app/views/__init__.py
+++ backend/app/views/__init__.py
@@ -21,8 +21,8 @@ product_router.add_url_rule("/update/<int:product_id>", view_func=product_view.u
 product_router.add_url_rule("/delete/<int:product_id>", view_func=product_view.delete_product_view, methods=["DELETE"])
 
 # Variant Endpoints
-variant_router.add_url_rule("", view_func=variant_view.get_variant_list_view, methods=["GET"])
+variant_router.add_url_rule("/list/<int:product_id>", view_func=variant_view.get_variant_list_view, methods=["GET"])
 variant_router.add_url_rule("/<int:variant_id>", view_func=variant_view.get_variant_view, methods=["GET"])
-variant_router.add_url_rule("/add", view_func=variant_view.create_variant_view, methods=["POST"])
+variant_router.add_url_rule("/add/<int:product_id>", view_func=variant_view.create_variant_view, methods=["POST"])
 variant_router.add_url_rule("/update/<int:variant_id>", view_func=variant_view.update_variant_view, methods=["PUT"])
 variant_router.add_url_rule("/delete/<int:variant_id>", view_func=variant_view.delete_variant_view, methods=["DELETE"])
