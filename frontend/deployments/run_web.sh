6d354d08f7261ca58601c07ed6ebc8bdaeb61679
danangjoyoo <agus.danangjoyo@gmail.com>
2022-12-13 10:56:30 +0700

    feat: adjust deployment

--- frontend/deployments/run_web.sh
+++ frontend/deployments/run_web.sh
@@ -3,9 +3,16 @@
 cd /base/app
 npm install -g npm@9.2.0
 npm install
-npm run build
+npm start
 
-cd /
-/usr/bin/supervisord
-sleep 5s
-tail -f /var/log/supervisor/supervisord.log
+
+## production not tested
+# cd /base/app
+# npm install -g npm@9.2.0
+# npm install
+# npm run build
+
+# cd /
+# /usr/bin/supervisord
+# sleep 5s
+# tail -f /var/log/supervisor/supervisord.log
